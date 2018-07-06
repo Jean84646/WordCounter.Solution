ac1227abbea3d083a74e9e0ceb2fd4a7e97ed7de
Jean Jia <Jean84646@gmail.com>
2018-07-06 15:26:44 -0700

    Controller HttpGet tests complete.

---
+++ WordCounter.Tests/ControllerTests/WordCounterController.cs
@@ -0,0 +1,49 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WordCounter.Controllers;
+using WordCounter.Models;
+
+namespace WordCounter.Tests
+{
+    [TestClass]
+    public class RepeatCountersControllerTest
+    {
+      [TestMethod]
+        public void Index_ReturnsCorrectView_True()
+        {
+            //Arrange
+            WordCounterController controller = new WordCounterController();
+
+            //Act
+            ActionResult indexView = controller.Index();
+
+            //Assert
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
+      [TestMethod]
+        public void Index_HasCorrectModelType_ItemList()
+        {
+            //Arrange
+            ViewResult indexView = new WordCounterController().Index() as ViewResult;
+
+            //Act
+            var result = indexView.ViewData.Model;
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(List<RepeatCounter>));
+        }
+      [TestMethod]
+        public void CreateForm_ReturnsCorrectView_True()
+        {
+            //Arrange
+            WordCounterController controller = new WordCounterController();
+
+            //Act
+            ActionResult indexView = controller.CreateForm();
+
+            //Assert
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
+    }
+}
